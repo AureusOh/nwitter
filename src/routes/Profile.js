0b8fb234e84d2e25a1393a3a4dd8fe7855098303
AureusOh <ohyoung79@gmail.com>
2022-06-04 06:06:39 +0100

    Final commit

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -13,40 +13,43 @@ const Profile = ({ userObj, refreshUser }) => {
 
   const onChange = (event) => {
     const {
-      target: {value},
+      target: { value },
     } = event;
     setNewDisplayName(value);
-  }
-
-  // const getMyNweets = async () => {
-  //   const nweets = await dbService
-  //                       .collection("nweets")
-  //                       .where("creatorId", "==", userObj.uid)
-  //                       .orderBy("createdAt", "asc")
-  //                       .get();
-  //   console.log(nweets.docs.map((doc)=>doc.data()));
-  // };
-
-  // useEffect(() => {
-  //   getMyNweets();
-  // }, []);
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    if (userObj.displayName != newDisplayName) {
+    if (userObj.displayName !== newDisplayName) {
       await userObj.updateProfile({ displayName: newDisplayName });
       refreshUser();
     }
   };
 
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} type="text" placeholder="Display Name" value={newDisplayName}/>
-        <input type="submit" value="Update Profile" />
+    <div className="container">
+      <form onSubmit={onSubmit} className="profileForm">
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+          autoFocus
+          className="formInput"
+        />
+        <input
+          type="submit"
+          value="Update Profile"
+          className="formBtn"
+          style={{
+            marginTop: 10,
+          }}
+        />
       </form>
-      <button onClick={onLogOutClick}>Log Out</button>
-    </>
+      <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+        Log Out
+      </span>
+    </div>
   );
 };
 
